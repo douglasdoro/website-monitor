ead7037f240da422e4434b371225f0079015d7cd
Douglas Fontes <douglasdoro@gmail.com>
2022-04-17 11:51:47 -0300

    add menu and option

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	displayWelcome()
+
+	for {
+		displayMenu()
+
+		option := readCommand()
+
+		switch option {
+		case 1:
+			fmt.Println("Option 1")
+		case 2:
+			fmt.Println("Option 2")
+		case 0:
+			fmt.Println("Exit program")
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid option!")
+			os.Exit(-1)
+		}
+	}
 }
 
 func displayWelcome() {
@@ -12,4 +34,20 @@ func displayWelcome() {
 
 	fmt.Println("Hello, " + name + "!")
 	fmt.Println("Program version: ", version)
+}
+
+func displayMenu() {
+	fmt.Println("")
+	fmt.Println("Choose an option:")
+	fmt.Println("1 - Start monitoring")
+	fmt.Println("2 - Display logs")
+	fmt.Println("0 - Signout")
+}
+
+func readCommand() int {
+	var command int
+
+	fmt.Scan(&command)
+
+	return command
 }
